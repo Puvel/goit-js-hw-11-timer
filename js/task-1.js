1fb698c0136cd2fc698e16d646340bc2cbc88b04
Puvel <puvelko@gmail.com>
2020-04-07 16:40:03 +0300

    timer

---
+++ js/task-1.js
@@ -0,0 +1,45 @@
+'use strict';
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.targetDate = targetDate;
+    this.selector = document.querySelector(`${selector}`);
+    this.days = this.selector.querySelector(`span[data-value="days"]`);
+    this.hours = this.selector.querySelector(`span[data-value="hours"]`);
+    this.mins = this.selector.querySelector(`span[data-value="mins"]`);
+    this.secs = this.selector.querySelector(`span[data-value="secs"]`);
+    this.firstTimer();
+    this.timer();
+  }
+
+  firstTimer() {
+    const difference = this.targetDate.getTime() - Date.now();
+    this.calculation(difference);
+  }
+
+  timer() {
+    this.timerId = setInterval(this.firstTimer.bind(this), 1000);
+  }
+
+  calculation(difference) {
+    this.days.textContent = Math.floor(difference / (1000 * 60 * 60 * 24));
+    this.hours.textContent = this.pad(
+      Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    this.mins.textContent = this.pad(
+      Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+    );
+    this.secs.textContent = this.pad(
+      Math.floor((difference % (1000 * 60)) / 1000),
+    );
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2020'),
+});
